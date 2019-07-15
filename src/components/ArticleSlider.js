bbffa21b6a1734b529ebdd4fa6629cda2d26c5b5
Yukako Sunabe <yukakosunabe@gmail.com>
2019-07-14 21:56:14 -0700

    Got posts title and fetured Images from Wordpress

--- src/components/ArticleSlider.js
+++ src/components/ArticleSlider.js
@@ -1,16 +1,102 @@
 import React from "react";
 
-const ArticleSlider = () => {
-    return (
-        <section className="top-image-section relative">
-            <div className="centralize">
-                <h2 className="top-image-section-title text-capitalize white font-weight-bold">
-                    latest blog title
-                </h2>
-                <p className="top-image-section-date white">May 20, 2019</p>
-            </div>
-        </section>
-    );
-};
+class ArticleSlider extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            postImages: []
+        };
+    }
+    componentDidMount() {
+        let postsURL = "http://localhost/wp-json/wp/v2/posts?_embed";
+        console.log(postsURL);
+
+        fetch(postsURL)
+            .then(response => response.json())
+            .then(response => {
+                this.setState({
+                    postImages: response
+                });
+            });
+    }
+    render() {
+        let postImage = this.state.postImages.map((postImage, index) => {
+            return (
+                <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                    // className="active"
+                />
+            );
+        });
+        console.log(postImage);
+
+        return (
+            <section className="top-image-section relative">
+                <div
+                    id="carouselExampleIndicators"
+                    className="carousel slide"
+                    data-ride="carousel"
+                >
+                    <ol className="carousel-indicators">
+                        {/* <li
+                            data-target="#carouselExampleIndicators"
+                            data-slide-to="0"
+                            className="active"
+                        />
+                        <li
+                            data-target="#carouselExampleIndicators"
+                            data-slide-to="1"
+                        /> */}
+                        {postImage}
+                    </ol>
+                    <div className="carousel-inner" role="listbox">
+                        <div className="carousel-item active">
+                            <img
+                                className="d-block w-100"
+                                src="#"
+                                data-src="holder.js/900x400?theme=social"
+                                alt="First slide"
+                            />
+                        </div>
+                        <div className="carousel-item">
+                            <img
+                                className="d-block w-100"
+                                src="#"
+                                data-src="holder.js/900x400?theme=industrial"
+                                alt="Second slide"
+                            />
+                        </div>
+                    </div>
+                    <a
+                        className="carousel-control-prev"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="prev"
+                    >
+                        <span
+                            className="carousel-control-prev-icon"
+                            aria-hidden="true"
+                        />
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a
+                        className="carousel-control-next"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="next"
+                    >
+                        <span
+                            className="carousel-control-next-icon"
+                            aria-hidden="true"
+                        />
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+            </section>
+        );
+    }
+}
 
 export default ArticleSlider;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Moment, Date } from "prismic-reactjs";
+import { Date } from "prismic-reactjs";
 // import { Link } from "react-router-dom";
 
 class PostList extends React.Component {
@@ -25,46 +25,81 @@ class PostList extends React.Component {
             const date = Date(post.date);
             var moment = require("moment");
             const formattedDate = moment(date).format("LL");
-
-            return (
-                <div
-                    key={index}
-                    className="card card-odd col-md-6 col-sm-12 border-0"
-                >
-                    <img
-                        className="card-img-top"
-                        src={
-                            post._embedded["wp:featuredmedia"][0].media_details
-                                .sizes["large"].source_url
-                        }
-                        alt="Card scap"
-                    />
-                    <div className="card-body">
-                        <h4 className="text-center alice card-title">
-                            {post.title.rendered}
-                        </h4>
-                        <h6 className="card-subtitle mb-2 alice text-center orange">
-                            {formattedDate}
-                        </h6>
-                        <p className="card-text">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </p>
-                        <a
-                            href="#!"
-                            className="orange text-uppercase font-weight-bold"
-                        >
-                            read more
-                        </a>
+            if (index === 0) {
+                return (
+                    <div
+                        key={index}
+                        className="card d-inline-block border-0 pl-3 pr-3 mb-3"
+                    >
+                        <div className="card-body">
+                            <h4 className="text-center alice card-title">
+                                {post.title.rendered}
+                            </h4>
+                            <h6 className="card-subtitle mb-2 alice text-center orange">
+                                {formattedDate}
+                            </h6>
+                        </div>
+                        <img
+                            className="card-img-top"
+                            src={
+                                post._embedded["wp:featuredmedia"][0]
+                                    .media_details.sizes["large"].source_url
+                            }
+                            alt="Card scap"
+                        />
+                        <div className="card-body">
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a
+                                href="#!"
+                                className="orange text-uppercase font-weight-bold"
+                            >
+                                read more
+                            </a>
+                        </div>
                     </div>
-                </div>
-            );
+                );
+            } else {
+                return (
+                    <div
+                        key={index}
+                        className="card d-inline-block border-0 w-50 pl-3 pr-3"
+                    >
+                        <img
+                            className="card-img-top"
+                            src={
+                                post._embedded["wp:featuredmedia"][0]
+                                    .media_details.sizes["large"].source_url
+                            }
+                            alt="Card scap"
+                        />
+                        <div className="card-body">
+                            <h4 className="text-center alice card-title">
+                                {post.title.rendered}
+                            </h4>
+                            <h6 className="card-subtitle mb-2 alice text-center orange">
+                                {formattedDate}
+                            </h6>
+                            <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                            </p>
+                            <a
+                                href="#!"
+                                className="orange text-uppercase font-weight-bold"
+                            >
+                                read more
+                            </a>
+                        </div>
+                    </div>
+                );
+            }
         });
-        return (
-            <div className="container mt-5">
-                <div className="row card-deck">{posts}</div>
-            </div>
-        );
+        return <div>{posts}</div>;
     }
 }
 export default PostList;
